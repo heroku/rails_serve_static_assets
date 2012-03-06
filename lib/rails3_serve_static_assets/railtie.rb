1c58f09c529baa1eeab9ffb649eb54ca8f5fb3ff
Jonathan Dance <jd@wuputah.com>
2012-03-06 15:54:29 -0800

    set setting in after_configuration block

--- lib/rails3_serve_static_assets/railtie.rb
+++ lib/rails3_serve_static_assets/railtie.rb
@@ -1,5 +1,7 @@
 module Rails3ServeStaticAssets
   class Railtie < Rails::Railtie
-    config.serve_static_assets = true
+    config.after_initialize do
+      ::Rails.configuration.serve_static_assets = true
+    end
   end
 end
