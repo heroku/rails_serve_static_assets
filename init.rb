84910ceb4ca2851d650e66d3b22f5008acf253f8
Jonathan Dance <jd@wuputah.com>
2012-03-14 18:03:32 -0700

    tweaking

--- init.rb
+++ init.rb
@@ -1,2 +1,2 @@
 # only run when used as a plugin
-Rails.application.class.config.serve_static_assets = true
+Rails.configuration.serve_static_assets = true
--- lib/rails3_serve_static_assets/railtie.rb
+++ lib/rails3_serve_static_assets/railtie.rb
@@ -1,6 +1,6 @@
 module Rails3ServeStaticAssets
   class Railtie < Rails::Railtie
-    config.after_initialize do
+    config.before_initialize do
       ::Rails.configuration.serve_static_assets = true
     end
   end
