a13603d2232706623d02fadba3ce48317cf07392
Adrian Mugnolo <adrian@wyeworks.com>
2014-12-22 18:52:23 -0200

    Fix conditional
    Use the right option with 4.2.0.

--- lib/rails_serve_static_assets/railtie.rb
+++ lib/rails_serve_static_assets/railtie.rb
@@ -1,7 +1,7 @@
 module RailsServeStaticAssets
   class Railtie < Rails::Railtie
     config.before_initialize do
-      if Rails.version > "4.2.0"
+      if Rails.version >= "4.2.0"
         ::Rails.configuration.serve_static_files = true
       else
         ::Rails.configuration.serve_static_assets = true
